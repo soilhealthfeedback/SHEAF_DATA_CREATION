dff7ef28812cab6473ae06f193c263715ff99e18
Erich Seamon <eseamon@sshgw02.research.sesync.org>
2019-01-15 16:34:55 -0500

    updated beta model semPath

--- SHEAF_SEM_beta1.R
+++ SHEAF_SEM_beta1.R
@@ -12,18 +12,21 @@ data <- read.csv("/nfs/soilsesfeedback-data/Model_data/WHEAT_Model_dataset1.csv"
 
 myModel <- '
 # measurement model
+
 Water_Availability =~ Drought + Heat
 Tillage =~ notill_farms + constill_farms
 Management =~ Residue.and.Tillage.Management..No.Till + Residue.and.Tillage.Management..Reduced.Till
-Practices =~ Conservation.Crop.Rotation + Cover.Crop
+Practices =~ Conservation.Crop.Rotation + Cover.Crop 
+
 # regressions
+
 Water_Availability ~ Tillage + Management
 Management ~ Practices + Water_Availability
 # residual correlations
 notill_farms ~~ constill_farms
-Drought ~ Heat
+Drought ~~ Heat
 Tillage ~~ Practices
-Conservation.Crop.Rotation ~~ Cover.Crop'
+Conservation.Crop.Rotation ~~ Cover.Crop '
 
 #y3 ~~ y7
 #y4 ~~ y8
@@ -33,11 +36,11 @@ Conservation.Crop.Rotation ~~ Cover.Crop'
 #fit <- cfa(model = myModel, data = data, missing = "fiml")
 #summary(fit, fit.measures = TRUE)
 
-fit <- sem(model = myModel, data=data)
+fit <- sem(model = myModel, data=data, std.lv =TRUE)
 
 #summary(fit, standardized=TRUE)
 
-semPaths(fit,what="std",layout="circle")
+semPaths(fit, "std", style = "mx", what = "cons", layout="tree2", curvePivot = TRUE, edge.label.cex=.7)
 
 
 
