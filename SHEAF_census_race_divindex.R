8639d0a75bdb227c50efeca810ad666f31cc252e
Erich Seamon <eseamon@sshgw02.research.sesync.org>
2019-02-27 21:19:11 -0500

    updated divindex file, fixing FIPS by merging ansi

--- SHEAF_census_race_divindex.R
+++ SHEAF_census_race_divindex.R
@@ -1,4 +1,3 @@
-library(pheatmap)
 library(dplyr)
 library(diverse)
 
@@ -18,16 +17,30 @@ census$State <- sapply(census$State, simpleCap)
 census$County <- tolower(census$County)
 census$County <- sapply(census$County, simpleCap)
 
-countyFIPS <- read.csv("https://nextcloud.sesync.org/index.php/s/wcFmKrSZW6Pr6D2/download")
+#convert ansi fields to integer for adding leading zeros
+census$State.ANSI <- as.integer(census$State.ANSI)
+census$County.ANSI <- as.integer(census$County.ANSI)
+
+#add leading zeros to make state 2 digits and county 3 digits
+census$State.ANSI <- sprintf("%02d",census$State.ANSI)
+census$County.ANSI <- sprintf("%03d",census$County.ANSI)
+
+#create a FIPS column
+census$FIPS <- paste(census$State.ANSI, census$County.ANSI, sep="")
 
 
-countyFIPS$FIPS <- sprintf("%05d",countyFIPS$FIPS)
-colnames(countyFIPS) <- c("ID", "State", "County", "FIPS")
 
-census2 <- plyr::join(census, countyFIPS, by  = c("State", "County"))
 
-census3 <- census2 %>%
-  select(FIPS, Year, Data.Item, Value) 
+#countyFIPS <- read.csv("https://nextcloud.sesync.org/index.php/s/wcFmKrSZW6Pr6D2/download")
+
+
+#countyFIPS$FIPS <- sprintf("%05d",countyFIPS$FIPS)
+#colnames(countyFIPS) <- c("ID", "State", "County", "FIPS")
+
+#census2 <- plyr::join(census, countyFIPS, by  = c("State", "County"))
+
+census3 <- census %>%
+  dplyr::select(FIPS, Year, Data.Item, Value) 
 census4 <- na.omit(census3) 
 census5 <- census4[ which(census4$Data.Item == c("OPERATORS, AMERICAN INDIAN OR ALASKA NATIVE - NUMBER OF OPERATORS", 
                                                  "OPERATORS, BLACK OR AFRICAN AMERICAN - NUMBER OF OPERATORS",
@@ -42,17 +55,17 @@ census5 <- census4[ which(census4$Data.Item == c("OPERATORS, AMERICAN INDIAN OR 
 census2002 <- census5[ which(census5$Year=='2002'), ] #select for only 2002  
                                                      #eliminate rows with NAs in FIPS
 census2002 <- census2002 %>%
-  select(FIPS, Data.Item, Value)                     #reduce to 3 variables
+  dplyr::select(FIPS, Data.Item, Value)                     #reduce to 3 variables
 
 census2007 <- census5[ which(census5$Year=='2007'), ] #select for only 2002  
 #eliminate rows with NAs in FIPS
 census2007 <- census2007 %>%
-  select(FIPS, Data.Item, Value)                     #reduce to 3 variables
+  dplyr::select(FIPS, Data.Item, Value)                     #reduce to 3 variables
 
 census2012 <- census5[ which(census5$Year=='2012'), ] #select for only 2002  
 #eliminate rows with NAs in FIPS
 census2012 <- census2012 %>%
-  select(FIPS, Data.Item, Value)                     #reduce to 3 variables
+  dplyr::select(FIPS, Data.Item, Value)                     #reduce to 3 variables
 
 div2002 <- diversity(census2002, type = "entropy")   #saves diversity measures as dataframe
 div2007 <- diversity(census2007, type = "entropy")   #entropy = the Shannon entropy per entity measure, denoted by "H"
@@ -70,7 +83,7 @@ div2002_div2007 <- rbind(div2002, div2007, by  = "FIPS")
 racediv <- rbind(div2002_div2007, div2012, by  = "FIPS")
 head(racediv)
 
-racediv[] <- lapply(racediv, function(x) as.numeric(as.character(x))) # convert to numberic variables
+racediv <- lapply(racediv, function(x) as.numeric(as.character(x))) # convert to numberic variables
 summary(racediv)
 
 write.csv(racediv, file = paste("/nfs/soilsesfeedback-data/data/census/racediversity.csv", sep=""))
